90a294cc23bce7787a904ceff68d033044e5117d
jianu1854 <jianu1854@gmail.com>
2025-09-20 18:42:39 +0530

    Update rankup.js

--- Priyansh/commands/rankup.js
+++ Priyansh/commands/rankup.js
@@ -52,12 +52,12 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
 		const moduleName = this.config.name;
 
     var background = [
-  "https://i.imgur.com/tVCXB0q.jpeg",
-  "https://i.imgur.com/JBYox72.jpeg",
-  "https://i.imgur.com/SRRuSRk.jpeg",   "https://i.imgur.com/qhx5HLz.jpeg",
-  "https://i.imgur.com/kbB4AfZ.jpeg",
-  "https://i.imgur.com/9oxlszW.jpeg",
-  "https://i.imgur.com/cJj8LTu.jpeg",   "https://i.imgur.com/LHb5eJt.jpeg",
+  "https://i.ibb.co/DD84WBF9/813902034295613.jpg",
+  "https://i.ibb.co/DD84WBF9/813902034295613.jpg",
+  "https://i.ibb.co/DD84WBF9/813902034295613.jpg",   "https://i.ibb.co/DD84WBF9/813902034295613.jpg",
+  "https://i.ibb.co/DD84WBF9/813902034295613.jpg",
+  "https://i.ibb.co/DD84WBF9/813902034295613.jpg",
+  "https://i.ibb.co/DD84WBF9/813902034295613.jpg",   "https://i.ibb.co/DD84WBF9/813902034295613.jpg",
 
   ];
     var rd = background[Math.floor(Math.random() * background.length)];
